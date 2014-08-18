0acfd9a33014052813a7850762ad35215839e3e0
John Louis Del Rosario <john2x@gmail.com>
2014-08-18 18:10:38 +0800

    Update defaults
    - _ is part of a word/symbol
    - allow y/n for yes/no prompt

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -1,7 +1,18 @@
 ;; for stuff that doesn't fit anywhere else
 
-(setq-default tab-width 4)
-
 (global-set-key (kbd "RET") 'newline-and-indent)
+
+;; make '_' part of a word
+(modify-syntax-entry  ?_ "w" (standard-syntax-table))
+
+(setq-default
+ tab-width 4
+ make-backup-files nil
+ indent-tabs-mode nil
+ show-trailing-whitespace t
+ visible-bell nil)
+
+;; allow entering y/n for yes/no prompt
+(fset 'yes-or-no-p 'y-or-n-p)
 
 (provide 'init-misc)
