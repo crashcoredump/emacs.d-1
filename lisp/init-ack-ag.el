532a6380825324a9b43833947676e3d0301d2c46
John Louis Del Rosario <john2x@gmail.com>
2014-09-18 21:23:50 +0800

    The silver searcher

--- lisp/init-ack-ag.el
+++ lisp/init-ack-ag.el
@@ -7,4 +7,8 @@
 (defalias 'ack-find-file 'ack-and-a-half-find-file)
 (defalias 'ack-find-file-same 'ack-and-a-half-find-file-same)
 
+(require-package 'ag)
+(require 'ag)
+(setq ag-highlight-search t)
+
 (provide 'init-ack)
